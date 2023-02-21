6052dc22bec2bf3d251d1a3e84c0e444694ee4e7
anirudhJoshi1112 <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-02-21 23:17:00 +0530

    Create pattern3.cpp
    triangle using while loop
    n=4
    *
    **
    ***
    ****

---
+++ pattern3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,count=1;
+    cin>>n;
+    i=1;
+
+    while(i<=n)
+    {
+        j=1;
+        while(j<=i)
+        {
+            cout<<"*";
+            
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
